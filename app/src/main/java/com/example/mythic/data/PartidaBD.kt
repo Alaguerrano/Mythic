781c665a5276f9202a1ca54d46ef35a57fe6dd0c
Alaguerrano <alaguerrano@gmail.com>
2021-11-25 10:46:16 +0100

    Mythic

---
+++ app/src/main/java/com/example/mythic/data/PartidaBD.kt
@@ -0,0 +1,34 @@
+package com.example.mythic.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Partida::class], version = 1, exportSchema = false)
+abstract class PartidaBD : RoomDatabase() {
+
+    abstract fun partidaDao() : PartidaDao
+
+    companion object{
+        @Volatile
+        val INSTANCIA : PartidaBD? = null
+
+        fun obtenerBD(context : Context) : PartidaBD{
+            val tempInstancia = INSTANCIA
+            if(tempInstancia =! null){
+                return tempInstancia
+            }
+            synchronized(this){
+                val instancia = Room.databaseBuilder(
+                    context.applicationContext,
+                    PartidaBD::class.java,
+                    "partidas_tabla"
+                ).build()
+                INSTANCIA = instancia
+                return instancia
+            }
+
+        }
+    }
+}
