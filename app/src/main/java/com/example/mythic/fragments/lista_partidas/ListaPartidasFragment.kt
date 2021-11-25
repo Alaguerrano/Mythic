f73c9bb9921f7f32210c19ab45470cd2768257c6
Alaguerrano <alaguerrano@gmail.com>
2021-11-25 22:00:52 +0100

    Mythic

--- app/src/main/java/com/example/mythic/fragments/lista_partidas/ListaPartidasFragment.kt
+++ app/src/main/java/com/example/mythic/fragments/lista_partidas/ListaPartidasFragment.kt
@@ -5,10 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.example.mythic.R
-
-
+import kotlinx.android.synthetic.main.fragment_lista_partidas.view.*
 
 
 class ListaPartidasFragment : Fragment() {
@@ -20,7 +20,11 @@ class ListaPartidasFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_insertar_partida, container, false)
+        val view = inflater.inflate(R.layout.fragment_lista_partidas, container, false)
+
+        view.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listaPartidasFragment_to_insertarPartidaFragment)
+        }
         return view
     }
 
