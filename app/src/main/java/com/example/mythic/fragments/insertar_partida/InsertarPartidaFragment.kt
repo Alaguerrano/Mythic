0da39c0cbce73594288f007dfdc645ac21d4d4b2
Alaguerrano <alaguerrano@gmail.com>
2021-11-25 13:59:20 +0100

    Mythic

--- app/src/main/java/com/example/mythic/fragments/insertar_partida/InsertarPartidaFragment.kt
+++ app/src/main/java/com/example/mythic/fragments/insertar_partida/InsertarPartidaFragment.kt
@@ -1,21 +1,14 @@
-package com.example.mythic
+package com.example.mythic.fragments.insertar_partida
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mythic.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InsertarPartidaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class InsertarPartidaFragment : Fragment() {
 
 
--- app/src/main/java/com/example/mythic/fragments/lista_partidas/ListaPartidasFragment.kt
+++ app/src/main/java/com/example/mythic/fragments/lista_partidas/ListaPartidasFragment.kt
@@ -1,23 +1,15 @@
-package com.example.mythic
+package com.example.mythic.fragments.lista_partidas
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mythic.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListaPartidasFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListaPartidasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
 
 
     override fun onCreateView(
