040e3610bfc96ce9f68c7623ae80b73adf6103b5
Alaguerrano <alaguerrano@gmail.com>
2021-12-03 00:46:09 +0100

    Mythic

--- app/src/main/java/com/example/mythic/model/Aventura.kt
+++ app/src/main/java/com/example/mythic/model/Aventura.kt
@@ -1,6 +1,11 @@
 package com.example.mythic.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "aventuras_tabla")
 data class Aventura(
+    @PrimaryKey(autoGenerate = true)
     val id : Int,
     val nombre : String,
     val premisa : String,
