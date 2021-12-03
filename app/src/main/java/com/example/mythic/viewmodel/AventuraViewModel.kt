24dee212855348fa2a63dc0f80c94156d455fd21
Alaguerrano <alaguerrano@gmail.com>
2021-12-03 01:05:33 +0100

    Mythic

---
+++ app/src/main/java/com/example/mythic/viewmodel/AventuraViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.mythic.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.mythic.data.aventura.AventuraBD
+import com.example.mythic.data.aventura.AventuraDao
+import com.example.mythic.model.Aventura
+import com.example.mythic.repository.AventuraRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AventuraViewModel (application: Application) : AndroidViewModel(application) {
+
+    val listaAventuras : LiveData<List<Aventura>>
+
+
+    private val repository : AventuraRepository
+    public val aventuraDao : AventuraDao
+
+    init{
+        aventuraDao = AventuraBD.obtenerBD(application).aventuraDao()
+        repository = AventuraRepository(aventuraDao)
+        listaAventuras = repository.listaAventuras
+
+
+    }
+
+    fun crearAventura (aventura: Aventura){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.crearAventura(aventura)
+        }
+    }
+
+    fun actualizarAventura (aventura: Aventura){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.actualizarAventura(aventura)
+        }
+    }
+
+    fun borrarAventura (aventura: Aventura){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.borrarAventura(aventura)
+        }
+    }
+
+
+}
