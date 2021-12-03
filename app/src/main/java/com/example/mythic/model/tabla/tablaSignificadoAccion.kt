bb0f8ce418574e23ad582845bce19cc34ae84171
Alaguerrano <alaguerrano@gmail.com>
2021-12-03 14:12:33 +0100

    Mythic

---
+++ app/src/main/java/com/example/mythic/model/tabla/tablaSignificadoAccion.kt
@@ -0,0 +1,8 @@
+package com.example.mythic.model.tabla
+
+class tablaSignificadoAccion {
+    val valores = arrayOf(
+        "lograr","Comenzar","Descuidar", "Luchar","Reclutar", "Triunfar", "Violar", "Oponerse a",
+
+    )
+}
